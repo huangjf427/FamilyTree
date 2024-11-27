885e20678fdd86821dd2e4bff8b19f3d99aa3d00
jhuang96 <huang.jf@163.com>
2024-11-27 12:22:58 +0800

    abc

--- familyTreeParsor.py
+++ familyTreeParsor.py
@@ -1,3 +1,14 @@
 import os
 import re
-import networkx
+ffreader = open('ff.txt',mode='r',encoding='utf-8')
+ffwriter = open('ffwriter.txt',mode='w',encoding='utf-8')
+lines = ffreader.readlines()
+newline = ' '
+for line in lines:
+    newline = re.search(r'^[\u4e00-\u9fa5]+：', line)
+    if newline:
+        ffwriter.write(line)
+ffreader.close()
+ffwriter.close()
+
+
